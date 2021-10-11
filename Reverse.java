68c246a25ac8946ce7c0d97d3c664b830cd00bbb
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-11 21:25:57 +0530

    Reverse No

---
+++ Reverse.java
@@ -0,0 +1,20 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Reverse {
+	public static void main(String[] args) {
+		 System.out.println("Enter no");
+		 Scanner sc= new Scanner(System.in);
+		 int n=sc.nextInt();
+		 int rev=0;
+		 while (n>0) {
+			int rem=n%10;
+			 rev=rev*10+rem;
+			 n=n/10;
+		  }
+		 System.out.println("After reversing "+rev);
+		 }
+	
+
+}
